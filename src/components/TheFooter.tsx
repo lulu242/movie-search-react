5adade2d2a1701f33a83e98c16beca4e1d74cb08
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-11-23 14:51:37 +0900

    feat: Footer

--- src/components/TheFooter.tsx
+++ src/components/TheFooter.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { myData } from '../types';
+import styled from 'styled-components';
 
 export const TheFooter: React.FC = () => {
   return (
-    <footer>footer</footer>
-  )
-}
+    <Footer>
+      <div>
+        <a href={myData.repository}>GitHub Repository.</a>
+      </div>
+      <div>
+        <a href={myData.github}>
+          {new Date().getFullYear()} 
+          YeongEun
+        </a>
+      </div>
+    </Footer>
+  );
+};
+
+const Footer = styled.footer`
+  padding: 40px 40px 60px;
+  text-align: center;
+  a {
+    color: var(--color-white-20);
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+`;
--- src/types/index.ts
+++ src/types/index.ts
@@ -16,6 +16,14 @@ export const routes = [
   {path: '/*', component: NotFound},
 ]
 
+export const myData = {
+  photo: 'https://velog.velcdn.com/images/heelieben/post/63766f3f-ffd7-445e-a7bc-41ba722c7cf0/image.png',
+  name: 'WonYeongEun',
+  email: 'woneun10@gmail.com',
+  blog: 'https://velog.io/@lulu',
+  github: 'https://github.com/lulu242',
+  repository: 'https://github.com/lulu242/movie-search-react'
+}
 export interface SimpleMovie {
   Title: string
   Year: string
@@ -62,3 +70,5 @@ export interface moviesState {
   message: string
   loading: boolean
 }
+
+
